c230b8556884ee33105267ed976aa8af74483a78
jhhbcz <jhhbcz@163.com>
2017-09-19 20:48:34 +0800

    maven版本控制

---
+++ java/com/ys/wg/service/UserService.java
@@ -0,0 +1,17 @@
+package com.ys.wg.service;
+
+import com.ys.wg.model.User;
+
+import java.util.List;
+
+/**
+ * Created by Zhangxq on 2016/7/15.
+ */
+public interface UserService {
+
+    List<User> getAllUser();
+
+    User getUserByPhoneOrEmail(String emailOrPhone, Short state);
+
+    User getUserById(Long userId);
+}
---
+++ java/com/ys/wg/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ys.wg.service.impl;
+
+import com.ys.wg.dao.UserDao;
+import com.ys.wg.model.User;
+import com.ys.wg.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by Zhangxq on 2016/7/15.
+ */
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class UserServiceImpl implements UserService {
+    
+    @Resource
+    private UserDao userDao;
+
+    public User getUserById(Long userId) {
+        return userDao.selectUserById(userId);
+    }
+    
+    public User getUserByPhoneOrEmail(String emailOrPhone, Short state) {
+        return userDao.selectUserByPhoneOrEmail(emailOrPhone,state);
+    }
+    
+    public List<User> getAllUser() {
+        return userDao.selectAllUser();
+    }
+}
